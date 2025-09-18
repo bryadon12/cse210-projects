696cf9bc51195247721f3110e8ecef8a95fa1942
Braydon Tolman <bctolman2@gmail.com>
2025-09-18 13:02:58 -0600

    added name to last print statement

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -12,6 +12,6 @@ class Program
         Console.WriteLine("Please enter the year you were born");
         int year = int.Parse(Console.ReadLine());
         Console.WriteLine($"Your number squared is {number * number}");
-        Console.WriteLine($"You turn {2025 - year} this year");
+        Console.WriteLine($"{name}, You turn {2025 - year} this year");
     }
 }
