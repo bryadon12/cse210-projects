fb842837d3af636733b14f1db3d3a9aafede62e2
Braydon Tolman <bctolman2@gmail.com>
2025-09-18 11:47:18 -0600

    Get last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,6 +6,9 @@ class Program
     {
         Console.WriteLine("What is your first name?");
         string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
     }
 }
 
