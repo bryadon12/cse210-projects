bc26dcf6f5e61db54c82df26f54d31ca134a402a
Braydon Tolman <bctolman2@gmail.com>
2025-09-18 12:45:10 -0600

    find largest number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,16 +8,26 @@ class Program
         List<int> numbers = new List<int>();
         int number;
         int sum = 0;
+        int large;
         do
         {
             Console.WriteLine("Enter Nmber: ");
             number = int.Parse(Console.ReadLine());
             numbers.Add(number);
         } while (number != 0);
+        large = number;
         foreach (int value in numbers)
         {
             sum += value;
+            if (value > large)
+            {
+                large = value;
+            }
         }
+
+        int average = sum / numbers.Count();
         Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {large}");
     }
 }
