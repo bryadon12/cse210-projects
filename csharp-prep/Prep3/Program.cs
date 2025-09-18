ad5c4234e980401cd596148e2f608440db0df72e
Braydon Tolman <bctolman2@gmail.com>
2025-09-18 12:20:36 -0600

    Ask for number and check guess

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Enter the magic number! (1-100)");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Guess the Magic Number!");
+        int guess = int.Parse(Console.ReadLine());
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("You Guessed It!!!");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Too Small!");
+        }
+        else
+        {
+            Console.WriteLine("Too Big");
+        }
     }
 }
