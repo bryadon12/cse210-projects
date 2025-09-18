2911c7bf8c503f1a24178f518ac6f0c01a948062
Braydon Tolman <bctolman2@gmail.com>
2025-09-18 12:00:52 -0600

    if else letterGrade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        string letterGrade;
         Console.WriteLine("Please enter a grade (0-100)");
         int grade = int.Parse(Console.ReadLine());
+        if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+        else if (grade < 70)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 80)
+        {
+            letterGrade = "C";
+        }
+        else if (grade < 90)
+        {
+            letterGrade = "B";
+        }
+        else
+        {
+            letterGrade = "A";
+        }
+        Console.WriteLine(letterGrade);
     }
 }
