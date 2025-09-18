94a59cbd737004cb22798c0653856e46e99bb3a4
Braydon Tolman <bctolman2@gmail.com>
2025-09-18 12:35:47 -0600

    Ask for a list of numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<int> numbers = new List<int>();
+        int number;
+        do
+        {
+            Console.WriteLine("Enter Nmber: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        } while (number != 0);
         Console.WriteLine("Hello Prep4 World!");
     }
 }
