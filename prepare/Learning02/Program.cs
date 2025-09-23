52bc19aa97d72db5acc4d36300427f0586b963fd
Braydon Tolman <bctolman2@gmail.com>
2025-09-23 11:36:06 -0600

    Test resume class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -18,5 +18,12 @@ class Program
 
         job1.Display();
         job2.Display();
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        Console.WriteLine(resume._jobs[1]);
+        Console.WriteLine(resume._jobs[2]);
     }
 }
