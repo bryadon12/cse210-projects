8c514b10e1960d59f180bbe5a7ff4d25a2acf818
Braydon Tolman <bctolman2@gmail.com>
2025-10-01 15:27:24 -0600

    fixed bugs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -9,6 +9,19 @@ class Program
         fraction1.setDenominator();
         fraction1.getDecimalValue();
         fraction1.getFractionString();
-        
+
+        Fraction fraction2 = new Fraction(5);
+        fraction2.getDecimalValue();
+        fraction2.getFractionString();
+
+        Fraction fraction3 = new Fraction(4 , 7);
+        fraction3.getDecimalValue();
+        fraction3.getFractionString();
+
+        fraction3.setNumerator();
+        fraction3.setDenominator();
+
+        fraction3.getDecimalValue();
+        fraction3.getFractionString();
     }
 }
