757d9728e69dab7aa789665c3b752fead65d1eb3
Braydon Tolman <bctolman2@gmail.com>
2025-10-02 13:03:27 -0600

    moved menu back to menu class

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -1,12 +1,83 @@
 public class Menu
 {
+    public Journal _journal;
+    public string _filename = "journal.txt";
+    public Menu(Journal journal)
+
+    {
+        _journal = journal;
+    }
     public void openMenu()
     {
         Console.WriteLine("1. Write New Entry\n2. Display Journal\n3. Save Journal\n4. Load Journal\n(Select 1-4, or 0 to exit)");
     }
 
-    public string selectOption()
+    public void selectOption()
     {
-        return Console.ReadLine();
+        int option = int.Parse(Console.ReadLine());
+
+        if (option == 1)
+        {
+            _journal.addEntry(writeEntry(_journal));
+        }
+        else if (option == 2)
+        {
+            displayJournal(_journal);
+        }
+        else if (option == 3)
+        {
+            saveJournal(_journal, _filename);
+        }
+        else if (option == 4)
+        {
+            loadJournal(_journal, _filename);
+        }
+        else
+        {
+            return;
+        }
+        openMenu();
+    }
+
+    public Entry writeEntry(Journal journal)
+    {
+        string prompt = journal.getPrompt();
+        Console.WriteLine(prompt);
+        string response = Console.ReadLine();
+        Console.WriteLine("Please Enter Today's Date: ");
+        string date = Console.ReadLine();
+        Entry entry = new Entry(date, response, prompt);
+        return entry;
+    }
+
+    public void displayJournal(Journal journal)
+    {
+        foreach (Entry entry in journal.getJournal())
+        {
+            Console.WriteLine(entry.getEntry());
+        }
+    }
+
+    public void saveJournal(Journal journal, string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in journal.getJournal())
+                outputFile.WriteLine(entry.getEntry());
+        }
+    }
+
+    public void loadJournal(Journal journal, string filename)
+    {
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(":");
+            string prompt = parts[0];
+            parts = parts[1].Split("(");
+            string response = parts[0];
+            string date = parts[1];
+            journal.addEntry(new Entry(date, response, prompt));
+        }
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,76 +3,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        string filename = "journal.txt";
+
         Journal journal = new Journal();
-        Menu menu = new Menu();
-        do
-        {
-            menu.openMenu();
-            int option = int.Parse(menu.selectOption());
-
-            if (option == 1)
-            {
-                journal.addEntry(writeEntry(journal)); 
-            }
-            else if (option == 2)
-            {
-                displayJournal(journal);
-            }
-            else if (option == 3)
-            {
-                saveJournal(journal, filename);
-            }
-            else if (option == 4)
-            {
-                loadJournal(journal, filename);
-            }
-            else
-            {
-                return;
-            }
-        } while (1 == 1);
-    }
-
-    public static Entry writeEntry(Journal journal)
-    {
-        string prompt = journal.getPrompt();
-        Console.WriteLine(prompt);
-        string response = Console.ReadLine();
-        Console.WriteLine("Please Enter Today's Date: ");
-        string date = Console.ReadLine();
-        Entry entry = new Entry(date, response, prompt);
-        return entry;
-    }
-
-    public static void displayJournal(Journal journal)
-    {
-        foreach (Entry entry in journal.getJournal())
-        {
-            Console.WriteLine(entry.getEntry());
-        }
-    }
-
-    public static void saveJournal(Journal journal, string filename)
-    {
-        using (StreamWriter outputFile = new StreamWriter(filename))
-        {
-            foreach (Entry entry in journal.getJournal())
-                outputFile.WriteLine(entry.getEntry());
-        }
-    }
-
-    public static void loadJournal(Journal journal, string filename)
-    {
-        string[] lines = System.IO.File.ReadAllLines(filename);
-        foreach (string line in lines)
-        {
-            string[] parts = line.Split(":");
-            string prompt = parts[0];
-            parts = parts[1].Split("(");
-            string response = parts[0];
-            string date = parts[1];
-            journal.addEntry(new Entry(date, response, prompt));
-        }
+        Menu menu = new Menu(journal);
+        menu.openMenu();
     }
 }
