f90447d76e426a9e665c46991d0bec7545b2fbae
Braydon Tolman <bctolman2@gmail.com>
2025-10-08 18:46:28 -0600

    created Assignment class

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,32 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment()
+    {
+        _studentName = "John Doe";
+        _topic = "School";
+    }
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public void SetName(string StudentName)
+    {
+        _studentName = StudentName;
+    }
+
+    public void SetTopic(string topic)
+    {
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"Name: {_studentName}, topic: {_topic}";
+    }
+}
