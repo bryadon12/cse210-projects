c242a16e54917499237472de0cf739d24d4f4210
Braydon Tolman <bctolman2@gmail.com>
2025-10-16 13:48:16 -0600

    Eternal Goal

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,11 @@
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, int points) : base(name, points)
+    {
+    }
+
+    public override int DoGoal()
+    {
+        return GetPoints();
+    }
+}
